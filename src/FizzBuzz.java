8929f5d237f962e0112eb96be52ee4760d842910
chowdberry <jawad.chowdhury@mail.utoronto.ca>
2023-09-11 18:15:12 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,12 +5,12 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
+        while (i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
-
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
 
@@ -29,6 +29,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
